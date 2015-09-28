b1739b773d3c520effddc448db868900e903fbcc
paul@gh <paul@glasshouse.fr>
2015-09-28 18:26:12 +0200

    Ajout du contenu des champs ACF sélectionnés pour l'analyse de contenu par Yoast

--- yoast-acf-glue.php
+++ yoast-acf-glue.php
@@ -13,12 +13,15 @@ include __DIR__ . '/fields/field.php';
 add_action("init",	"yoast_acf_glue", 10, 1);
 function yoast_acf_glue()
 {
-    
-    include __DIR__ . '/fields/text.php';
+    if (is_admin()) {
 
-    do_action('yoast-acf-glue/include');
+        include __DIR__ . '/fields/text.php';
 
-    add_filter('wpseo_pre_analysis_post_content', 'yoast_acf_add_content_to_yoast', 10, 2);
+        do_action('yoast-acf-glue/include');
+
+        add_filter('wpseo_pre_analysis_post_content', 'yoast_acf_add_content_to_yoast', 10, 2);
+
+    }
 
 }
 
@@ -42,36 +45,79 @@ function yoast_acf_add_content_to_yoast( $content )
         );
 
         foreach( $field_groups as $i => $field_group ) {
-
-
-            $seo_fields[$field_group['ID']] = array();
             // visibility
             $visibility = acf_get_field_group_visibility( $field_group, $args);
 
             if ($visibility) {
                 $group_fields = acf_get_fields($field_group);
                 if (!empty($group_fields && is_array($group_fields))) {
-                    foreach ($group_fields as $gfield) {
-//                        var_dump($gfield);
-                        if (!empty($gfield['yoast_analysis']) && $gfield['yoast_analysis'][0] === 'yes') {
-                            $seo_fields[$field_group['ID']][$gfield['name']] = $gfield['type'];
-                        }
-                    }
+                    scan_prepare_fields($field_group, $group_fields, $seo_fields);
                 }
-            }
-
-            if (empty($seo_fields[$field_group['ID']])) {
-                unset($seo_fields[$field_group['ID']]);
             }
         }
     }
 
     if (!empty($seo_fields)) {
-        var_dump($seo_fields);
+//        var_dump($seo_fields);
         $fields = get_fields($pid);
+//        var_dump($fields);
+
+        foreach($fields as $key => $field) {
+            scan_add_field($key, $field, $fields, $seo_fields, $content);
+        }
     }
 
-//    var_dump(get_fields($pid));
+//    remove_filter('wpseo_pre_analysis_post_content', 'yoast_acf_add_content_to_yoast', 10);
     
 	return $content;
+}
+
+function scan_prepare_fields($parent, $fields, &$seo_fields = array())
+{
+    $tmp = array();
+
+    foreach ($fields as $gfield) {
+//        var_dump($gfield);
+        if (!empty($gfield['yoast_analysis']) && $gfield['yoast_analysis'][0] === 'yes') {
+            $tmp[$gfield['name']] = $gfield['type'];
+        }
+
+        if (!empty($gfield['sub_fields'])) {
+            scan_prepare_fields($gfield, $gfield['sub_fields'], $tmp);
+        }
+        else if (!empty($gfield['layouts'])) {
+            foreach ($gfield['layouts'] as $layout) {
+//                scan_prepare_fields($layout, $gfield['sub_fields'], $tmp);
+                if (!empty($layout['sub_fields'])) {
+                    scan_prepare_fields($gfield, $layout['sub_fields'], $tmp);
+                }
+            }
+        }
+    }
+
+    if (!empty($tmp)) {
+        if (isset($parent['name'])) {
+            $seo_fields[$parent['name']] = $tmp;
+        }
+        else {
+            $seo_fields = $tmp;
+        }
+    }
+}
+
+function scan_add_field($key, $field, $fields, $seo_fields, &$content)
+{
+    if (!is_array($field) && isset($seo_fields[$key]) && !is_array($seo_fields[$key])) {
+        $content .= ' ' . $field;
+    }
+
+    if (!empty($field) && is_array($field) && !empty($seo_fields[$key])) {
+        foreach ($field as $subfields) {
+            foreach($subfields as $subkey => $subfield) {
+                scan_add_field($subkey, $subfield, $subfields, $seo_fields[$key], $content);
+            }
+        }
+    }
+
+//    var_dump($key, $field);
 }
